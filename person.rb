eb68ffddb795d6d049b5eb8c1286d5dcb71e8dbe
krisbobo <kiyongbohbo@gmail.com>
2023-05-27 21:48:14 +0100

    Create 'many-to-many' relationship between Person and Book
    Use the Rental intermediate class. The following are created: a book has many rentals(`has-many` side of `Book` and `Rental`; a rental belongs to a book; a person has many rentals; a rental belongs to a person.

--- person.rb
+++ person.rb
@@ -1,25 +1,35 @@
 require_relative 'nameable'
 require_relative 'capitalize_decorator'
 require_relative 'trimmer_decorator'
+require_relative 'rental'
 
 class Person < Nameable
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals
   attr_reader :id
 
-  def initialize(age, parent_permission: true, name: 'Unknown')
+  def initialize(age, name: 'Unknown', parent_permission: true)
     super()
     @id = generate_random_id
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def correct_name
     name
   end
 
+  def add_rental(rental)
+    @rentals << rental
+  end
+
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def to_s
+    "Person: #{name} is #{age} years old"
   end
 
   private
@@ -32,12 +42,3 @@ class Person < Nameable
     @age >= 18
   end
 end
-
-person = Person.new(22, name: 'maximilianus')
-puts person.correct_name
-
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
--- student.rb
+++ student.rb
@@ -1,12 +1,25 @@
-class Student < Person
-  attr_accessor :classroom
+require_relative 'person'
+require_relative 'classroom'
 
-  def initialize(age, classroom, parent_permission: true, name: 'Unknown')
-    super(age, parent_permission: parent_permission, name: name)
+class Student < Person
+  attr_accessor :name
+  attr_reader :classroom
+
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+    super(age, name: name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.add_student.push(self) unless classroom.students.include?(self)
+  end
+
+  def to_s
+    "Student: #{name}"
+  end
+
   def play_hooky
-    '¯\\(ツ)/¯'
+    '¯\(ツ)/¯'
   end
 end
