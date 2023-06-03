f7e6cad10fa0101274d3219b62d89ba8f41fe2b6
krisbobo <kiyongbohbo@gmail.com>
2023-06-03 01:37:54 +0100

    Initialize teacher

--- teacher.rb
+++ teacher.rb
@@ -1,9 +1,11 @@
 class Teacher < Person
   attr_accessor :specialization
+  attr_reader :type
 
   def initialize(age, specialization, parent_permission: true, name: 'Unknown')
     super(age, parent_permission: parent_permission, name: name)
     @specialization = specialization
+    @type = 'Teacher'
   end
 
   def can_use_services?
